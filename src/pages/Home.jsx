6362f22aa2988161722fc434b0b4a48efb8d2e79
NicoleGluj <nicolegluj@gmail.com>
2025-11-07 17:42:35 -0300

    fixed: link boton home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { Layout } from "../layout/Layout";
+import { useAuth } from "../context/AuthContext";
 
 export const Home = () => {
+  const { user } = useAuth()
+
   return (
     <Layout>
       <section className="flex flex-col items-start justify-end min-h-[75vh] xl:p-6 p-4">
@@ -9,7 +12,7 @@ export const Home = () => {
           Conoce una nueva forma de recordar tus tareas
         </h1>
         <Link
-          to="/login"
+          to={user ? "/mistareas" : "/login"}
           className="group relative inline-flex items-center justify-center border-2 border-white text-white px-8 py-2 rounded-2xl mt-8 font-medium uppercase text-lg sm:text-base overflow-hidden"
         >
           <span className="z-10">Comenzar</span>
